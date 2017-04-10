c62779091612cd77aec3d765f052a7325fb59818
Jaehoon Jung <jung032111@gmail.com>
2017-04-10 16:45:39 +0900

    Updates
    - admin announcement (delete post)
    - announcement to all (main page)

--- announcement.php
+++ announcement.php
@@ -147,6 +147,52 @@
     </nav>
 
 
+    <!--Code Start-->
+
+
+    <div class="container">
+         <table>
+           <h1>Announcement</h1>
+         <thead>
+           <tr>
+             <th>Number</th>
+             <th>Title</th>
+             <th>Author</th>
+             <th>Date</th>
+             <th>Hit</th>
+           </tr>
+         </thead>
+         <tbody>
+
+
+           <?php
+                $sql = 'select * from announcement order by board_no desc';
+                $result = $dbconn->query($sql);
+                while($row = $result->fetch_assoc())
+                {
+                  $datetime = explode(' ', $row['board_date']);
+                  $date = $datetime[0];
+                  $time = $datetime[1];
+                  if($date == Date('Y-m-d'))
+                  $row['board_date'] = $time;
+                  else
+                  $row['board_date'] = $date;
+                  ?>
+                  <tr>
+                    <td><?php echo $row['board_no']?></td>
+                    <td><a href="./adminAnnoun_view.php?bno=<?php echo $row['board_no']?>"><?php echo $row['board_title']?></a></td>
+                    <td><?php echo $row['board_admin']?></td>
+                    <td><?php echo $row['board_date']?></td>
+                    <td><?php echo $row['board_hit']?></td>
+                  </tr>
+                  <?php
+                }
+                ?>
+         </tbody>
+       </table>
+     </div>
+
+
 
 
 
