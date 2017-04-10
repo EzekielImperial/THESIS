eebc4c7d806500449b0d453f9027924f7477cde3
Jaehoon Jung <jung032111@gmail.com>
2017-04-10 20:47:38 +0900

    Update
    - Seller can choose gender(man,woman)
    - Db in product updated

---
+++ jsmain/jsmain.js
@@ -0,0 +1,8 @@
+function onSelectChange() {
+    var value = document.getElementById("category").value;
+    if ( (value == 'Clothing and Accessories') || (value == 'Bags and Accessories')) {
+        document.getElementById('gender').style.display = 'block';
+    } else {
+        document.getElementById('gender').style.display = 'none';
+    }
+}
