1e526ebf52ae8aa3543361d26ba85a6b46b51ace
khellytaguinod <khellytaguinod@gmail.com>
2017-04-11 18:16:44 +0800

    Now has ProductPage whohoo!
    *productPage1.php ( can now show product page per specific product )
    - super fck up code
    - working dynamic tabs under description
    - temp add to bag pictures hahaha

--- productView.php
+++ productView.php
@@ -34,74 +34,73 @@
 
 
       <nav id="navbar-main">
-        <!--Login System Embedded by Jung Start-->
-          <div class="container">
-              <div class="navbar-header">
-                  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-    <span class="icon-bar"></span>
-    <span class="icon-bar"></span>
-    <span class="icon-bar"></span>
-    </button>
-              </div>
-              <div class="collapse navbar-collapse row" id="myNavbar">
-                  <ul class="pull-right">
-                      <?php if(isset($_SESSION['email'])&& $_SESSION['userType'] == 'employee'){ ?>
-                      <li class="upper-links"><a class="links" href="productAdd.php"><span class="glyphicon glyphicon-upload" aria-hidden="true"></span> SELL</a></li>
-                      <li class="upper-links"><a class="links" href="#"><span class="glyphicon glyphicon-bell" aria-hidden="true"></span> NOTIFICATIONS</a></li>
-                      <li class="upper-links"><a class="links" href="#"><span class="glyphicon glyphicon-heart" aria-hidden="true"></span> WISHLIST</a></li>
-                      <li class="upper-links"><a class="links" href="#"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> CART</a></li>
+          <!--Login System Embedded by Jung Start-->
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      </button>
+                </div>
+                <div class="collapse navbar-collapse row" id="myNavbar">
+                    <ul class="pull-right">
+                        <?php if(isset($_SESSION['email'])&& $_SESSION['userType'] == 'employee'){ ?>
+                        <li class="upper-links"><a class="links" href="productAdd.php"><span class="glyphicon glyphicon-upload" aria-hidden="true"></span> SELL</a></li>
+                        <li class="upper-links"><a class="links" href="#"><span class="glyphicon glyphicon-bell" aria-hidden="true"></span> NOTIFICATIONS</a></li>
+                        <li class="upper-links"><a class="links" href="index_wishlist.php"><span class="glyphicon glyphicon-heart" aria-hidden="true"></span> WISHLIST</a></li>
+                        <li class="upper-links"><a class="links" href="index_shopcart.php"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> CART</a></li>
 
-                      <li class="upper-links dropdown"><a class="links">My Account</a>
-                          <ul class="dropdown-menu">
-                              <li class="profile-li"><a class="profile-links" href="#">My Order</a></li>
-                              <li class="profile-li"><a class="profile-links" href="accountSetting.php">Account Setting</a></li>
-                              <li class="profile-li"><a class="profile-links" href="#">Change Password </a></li>
-                              <li class="profile-li"><a class="profile-links" href="logout.php">logout</a></li>
+                        <li class="upper-links dropdown"><a class="links">My Account</a>
+                            <ul class="dropdown-menu">
+                                <li class="profile-li"><a class="profile-links" href="#">My Order</a></li>
+                                <li class="profile-li"><a class="profile-links" href="accountSetting.php">Account Setting</a></li>
+                                <li class="profile-li"><a class="profile-links" href="#">Change Password </a></li>
+                                <li class="profile-li"><a class="profile-links" href="logout.php">logout</a></li>
 
-                              <?php }elseif(isset($_SESSION['email'])&& $_SESSION['userType'] == 'student'){ ?>
-                              <li class="upper-links"><a class="links" href="productAdd.php"><span class="glyphicon glyphicon-upload" aria-hidden="true"></span> SELL</a></li>
-                              <li class="upper-links"><a class="links" href="#"><span class="glyphicon glyphicon-bell" aria-hidden="true"></span> NOTIFICATIONS</a></li>
-                              <li class="upper-links"><a class="links" href="#"><span class="glyphicon glyphicon-heart" aria-hidden="true"></span> WISHLIST</a></li>
-                              <li class="upper-links"><a class="links" href="#"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> CART</a></li>
+                                <?php }elseif(isset($_SESSION['email'])&& $_SESSION['userType'] == 'student'){ ?>
+                                <li class="upper-links"><a class="links" href="productAdd.php"><span class="glyphicon glyphicon-upload" aria-hidden="true"></span> SELL</a></li>
+                                <li class="upper-links"><a class="links" href="#"><span class="glyphicon glyphicon-bell" aria-hidden="true"></span> NOTIFICATIONS</a></li>
+                                <li class="upper-links"><a class="links" href="index_wishlist.php"><span class="glyphicon glyphicon-heart" aria-hidden="true"></span> WISHLIST</a></li>
+                                <li class="upper-links"><a class="links" href="index_shopcart.php"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> CART</a></li>
 
-                              <li class="upper-links dropdown"><a class="links">My Account</a>
-                                  <ul class="dropdown-menu">
-                                      <li class="profile-li"><a class="profile-links" href="#">My Order</a></li>
-                                      <li class="profile-li"><a class="profile-links" href="accountSetting.php">Account Setting</a></li>
-                                      <li class="profile-li"><a class="profile-links" href="#">Change Password </a></li>
-                                      <li class="profile-li"><a class="profile-links" href="logout.php">logout</a></li>
+                                <li class="upper-links dropdown"><a class="links">My Account</a>
+                                    <ul class="dropdown-menu">
+                                        <li class="profile-li"><a class="profile-links" href="#">My Order</a></li>
+                                        <li class="profile-li"><a class="profile-links" href="accountSetting.php">Account Setting</a></li>
+                                        <li class="profile-li"><a class="profile-links" href="#">Change Password </a></li>
+                                        <li class="profile-li"><a class="profile-links" href="logout.php">logout</a></li>
 
-                                      <?php }elseif(isset($_SESSION['email'])&& $_SESSION['userType'] == 'admin'){ ?>
-                                      <li class="upper-links"><a class="links" href="productAdd.php"><span class="glyphicon glyphicon-upload" aria-hidden="true"></span> SELL</a></li>
-                                      <li class="upper-links"><a class="links" href="#"><span class="glyphicon glyphicon-bell" aria-hidden="true"></span> NOTIFICATIONS</a></li>
-                                      <li class="upper-links"><a class="links" href="#"><span class="glyphicon glyphicon-heart" aria-hidden="true"></span> WISHLIST</a></li>
-                                      <li class="upper-links"><a class="links" href="#"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> CART</a></li>
+                                        <?php }elseif(isset($_SESSION['email'])&& $_SESSION['userType'] == 'admin'){ ?>
+                                        <li class="upper-links"><a class="links" href="productAdd.php"><span class="glyphicon glyphicon-upload" aria-hidden="true"></span> SELL</a></li>
+                                        <li class="upper-links"><a class="links" href="#"><span class="glyphicon glyphicon-bell" aria-hidden="true"></span> NOTIFICATIONS</a></li>
+                                        <li class="upper-links"><a class="links" href="index_wishlist.php"><span class="glyphicon glyphicon-heart" aria-hidden="true"></span> WISHLIST</a></li>
+                                        <li class="upper-links"><a class="links" href="index_shopcart.php"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> CART</a></li>
 
-                                      <li class="upper-links dropdown"><a class="links">My Account</a>
-                                          <ul class="dropdown-menu">
-                                              <li class="profile-li"><a class="profile-links" href="adminPart/adminDashboard.php">Admin Dashboard</a></li>
-                                              <li class="profile-li"><a class="profile-links" href="accountSetting.php">Account Setting</a></li>
-                                              <li class="profile-li"><a class="profile-links" href="#">Change Password </a></li>
-                                              <li class="profile-li"><a class="profile-links" href="logout.php">logout</a></li>
-
-
-                              <?php }else { ?>
-                              <li class="upper-links dropdown"><a class="links">My Account</a>
-                                  <ul class="dropdown-menu">
-                                      <li class="profile-li"><a class="profile-links" href="login.php">LOGIN</a></li>
-                                      <li class="profile-li"><a class="profile-links" href="signUp.php">REGISTER</a></li>
-                                      <?php } ?>
+                                        <li class="upper-links dropdown"><a class="links">My Account</a>
+                                            <ul class="dropdown-menu">
+                                                <li class="profile-li"><a class="profile-links" href="#">My Order</a></li>
+                                                <li class="profile-li"><a class="profile-links" href="accountSetting.php">Account Setting</a></li>
+                                                <li class="profile-li"><a class="profile-links" href="#">Change Password </a></li>
+                                                <li class="profile-li"><a class="profile-links" href="logout.php">logout</a></li>
 
 
-                                  </ul>
-                              </li>
-                          </ul></ul></ul>
-                  </ul>
-              </div>
-          </div>
-          <!--Login System Embedded by Jung End-->
+                                <?php }else { ?>
+                                <li class="upper-links dropdown"><a class="links">My Account</a>
+                                    <ul class="dropdown-menu">
+                                        <li class="profile-li"><a class="profile-links" href="login.php">LOGIN</a></li>
+                                        <li class="profile-li"><a class="profile-links" href="signUp.php">REGISTER</a></li>
+                                        <?php } ?>
 
 
+                                    </ul>
+                                </li>
+                            </ul></ul></ul>
+                    </ul>
+                </div>
+            </div>
+            <!--Login System Embedded by Jung End-->
+    
 
           <div class="row">
               <!--Size-->
@@ -109,7 +108,7 @@
               </div>
               <!--Size-->
               <div class="col-sm-1">
-                  <a href="index.php"><img src="image/logo.png" width="70px" height="70px"></a>
+                  <a href="index.php"><img sizes="" rc="image/logo.png" width="70px" height="70px"></a>
               </div>
               <div class="smallsearch col-sm-8 col-xs-11">
                   <div class="row">
@@ -174,12 +173,15 @@
 
                              while($row = mysqli_fetch_array($results)){
 
-                                 echo '
+                                 echo'
                                  <div class ="proBox1">
 
                                  <img src="productImages/' .$row['productImage']. '" width="60%" height="60%"/>
                                  <br>
-                                    '.$row['productName'].' <br />
+
+                                 <a href="productPage1.php?pname='.$row['productName'].'" style="color:black; text-decoration:none;";>'.$row['productName'].'</a> <br> 
+
+                          
                                     '.$row['shortDes'].' <br />
                                   ₱ '.$row['price'].'
                                  <br>
